7adad9d539e7971838400da41b3d543a0d9e8687
Potter Dai <daizhongyuan@gmail.com>
2017-01-01 17:18:43 -0500

    Initial Commit

---
+++ src/com/potter/riderunity3dconnector/UnityConnectorServer.java
@@ -0,0 +1,112 @@
+package com.potter.riderunity3dconnector;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class UnityConnectorServer {
+
+    public static final UnityConnectorServer INSTANCE = new UnityConnectorServer();
+    private ServerRunnable serverRunnable;
+    private ExecutorService executorService;
+    private volatile DatagramSocket datagramSocket;
+    private int port = 11234;
+    private Project project;
+
+    private UnityConnectorServer() {
+        executorService = Executors.newSingleThreadExecutor();
+    }
+
+    public static UnityConnectorServer getInstance() {
+        return INSTANCE;
+    }
+
+    public boolean startServer(int port, Project project) {
+        this.project = project;
+        if (serverRunnable != null && serverRunnable.isRunning()) {
+            // Server is already running
+            return false;
+        }
+        try {
+            datagramSocket = new DatagramSocket(port);
+            this.port = port;
+            serverRunnable = new ServerRunnable();
+            executorService.execute(serverRunnable);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void stopServer() {
+        if (serverRunnable == null) {
+            // Server is not started
+            return;
+        }
+        serverRunnable.stop();
+    }
+
+    public boolean isRunning() {
+        if (serverRunnable == null) {
+            return false;
+        }
+        return serverRunnable.isRunning();
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    private class ServerRunnable implements Runnable {
+        private boolean isRunning = true;
+
+        @Override
+        public void run() {
+            while (isRunning) {
+                try {
+                    byte[] data = new byte[1024];
+                    DatagramPacket datagramPacket = new DatagramPacket(data, data.length);
+                    datagramSocket.receive(datagramPacket);
+                    data = datagramPacket.getData();
+                    String text = new String(data);
+                    final String[] splits = text.split("\r\n");
+                    ApplicationManager.getApplication().invokeLater(() -> {
+                        VirtualFile vf = LocalFileSystem.getInstance().findFileByPath(splits[2]);
+
+                        int line = Integer.parseInt(splits[0]) - 1;
+                        if (line < 0) line = 0;
+
+                        int column = 0;
+                        if (splits.length == 4) {
+                            column = Integer.parseInt(splits[0]) - 1;
+                            if (column < 0) column = 0;
+                        }
+
+                        new OpenFileDescriptor(project, vf, line, column).navigate(true);
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    stop();
+                }
+            }
+        }
+
+        public synchronized void stop() {
+            isRunning = false;
+            datagramSocket.close();
+        }
+
+        public synchronized boolean isRunning() {
+            return isRunning;
+        }
+    }
+}
---
+++ src/com/potter/riderunity3dconnector/UnityConnectorSettingsAction.java
@@ -0,0 +1,22 @@
+package com.potter.riderunity3dconnector;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+
+public class UnityConnectorSettingsAction extends AnAction {
+    public void actionPerformed(AnActionEvent event) {
+        Project project = event.getData(PlatformDataKeys.PROJECT);
+
+        SettingsDialog settingsDialog = new SettingsDialog(project, true);
+        settingsDialog.show();
+
+        int exitCode = settingsDialog.getExitCode();
+
+        if(exitCode == SettingsDialog.OK_EXIT_CODE) {
+        } else {
+            settingsDialog.stopListening();
+        }
+    }
+}
