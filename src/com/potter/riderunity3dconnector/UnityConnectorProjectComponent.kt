cc54dd2e852db7210e590f0a7b7fb1c844dbd467
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2017-02-08 23:13:57 +0100

    Revert "move to package"
    This reverts commit 696c4ec48bf7927011b49df829b143c1d496b11e.

--- src/com/potter/riderunity3dconnector/UnityConnectorProjectComponent.kt
+++ src/com/potter/riderunity3dconnector/UnityConnectorProjectComponent.kt
@@ -1,14 +1,9 @@
-package com.potter.riderunity3dconnector.runConfig
+package com.potter.riderunity3dconnector
 
 import com.intellij.execution.RunManagerEx
 import com.intellij.openapi.components.AbstractProjectComponent
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.openapi.vfs.VirtualFileAdapter
-import com.intellij.openapi.vfs.VirtualFileEvent
-import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.util.io.isFile
-import com.jetbrains.resharper.projectView.index.VirtualFileIndex
 import com.jetbrains.resharper.run.configurations.remote.DotNetRemoteConfiguration
 import com.jetbrains.resharper.run.configurations.remote.DotNetRemoteConfigurationFactory
 import com.jetbrains.resharper.run.configurations.remote.MonoRemoteConfigType
@@ -25,25 +20,26 @@ import javax.xml.xpath.XPathFactory
 
 /**
  * Created by Ivan.Shakhov on 03.02.2017.
- * Todo: if Unity process is restarted, but Rider is not, runconfiguration must be updated.
  */
 
 
 class UnityConnectorProjectComponent(val project: Project) : AbstractProjectComponent(project), ILifetimedComponent by LifetimedComponent(project) {
+    companion object {
+        val logger = getLogger(this)
+    }
     override fun projectOpened() {
 
-        val optionalCsprojFile = Files.walk(File(project.baseDir.path).toPath()).filter({
+        val csprojFile = Files.walk(File(project.baseDir.path).toPath()).filter({
             it.isFile() && it.toFile().extension == "csproj"
                     && it.toFile().readText().contains("UnityEngine.dll") && it.toFile().readText().contains("<unityProcessId>")
         }).findFirst()
-        if (optionalCsprojFile.isPresent) {
-            val csprojFile = optionalCsprojFile.get()
-            val pid = XPathFactory.newInstance().newXPath().compile("/Project/PropertyGroup/unityProcessId")
+        if (csprojFile.isPresent) {
+            val pid =
+                    XPathFactory.newInstance().newXPath().compile("/Project/PropertyGroup/unityProcessId")
                             .evaluate(DocumentBuilderFactory.newInstance()
-                                    .newDocumentBuilder().parse(csprojFile.toFile()), XPathConstants.NUMBER) as Double?
+                                    .newDocumentBuilder().parse(csprojFile.get().toFile()), XPathConstants.NUMBER) as Double?
 
             if (pid != null) {
-
                 val unityConfig = UnityLocalAttachConfiguration("temp", pid.toInt())
                 var runManager = RunManagerEx.getInstance(project)
                 var config = runManager.createRunConfiguration("unity-debug-generated", DotNetRemoteConfigurationFactory(MonoRemoteConfigType()))
@@ -52,19 +48,9 @@ class UnityConnectorProjectComponent(val project: Project) : AbstractProjectComp
                 config.isTemporary = true
                 runManager.addConfiguration(config, false)
                 runManager.selectedConfiguration = config
-
-
-                VirtualFileManager.getInstance().addVirtualFileListener(object : VirtualFileAdapter() {
-                    val fileToWatch = LocalFileSystem.getInstance().findFileByPath(csprojFile!!.toString())
-                    override fun contentsChanged(event: VirtualFileEvent) {
-                        if (event.file == fileToWatch)
-                          (config.configuration as DotNetRemoteConfiguration).port =5000
-                    }
-                })
             }
         }
     }
-
     override fun getComponentName(): String {
         return "UnityConnectorProjectComponent"
     }
