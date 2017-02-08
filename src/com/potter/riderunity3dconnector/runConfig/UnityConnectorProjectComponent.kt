cc0cf74de38b3bfef7a2a37dc55b45c6faa54431
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2017-02-08 23:16:18 +0100

    move to package, cleanup

--- src/com/potter/riderunity3dconnector/runConfig/UnityConnectorProjectComponent.kt
+++ src/com/potter/riderunity3dconnector/runConfig/UnityConnectorProjectComponent.kt
@@ -1,4 +1,4 @@
-package com.potter.riderunity3dconnector
+package com.potter.riderunity3dconnector.runConfig
 
 import com.intellij.execution.RunManagerEx
 import com.intellij.openapi.components.AbstractProjectComponent
@@ -10,23 +10,13 @@ import com.jetbrains.resharper.run.configurations.remote.MonoRemoteConfigType
 import com.jetbrains.resharper.run.configurations.remote.Unity.UnityLocalAttachConfiguration
 import com.jetbrains.resharper.util.idea.ILifetimedComponent
 import com.jetbrains.resharper.util.idea.LifetimedComponent
-import com.jetbrains.resharper.util.idea.getLogger
 import java.io.File
 import java.nio.file.Files
-import java.nio.file.Path
 import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.xpath.XPathConstants
 import javax.xml.xpath.XPathFactory
 
-/**
- * Created by Ivan.Shakhov on 03.02.2017.
- */
-
-
 class UnityConnectorProjectComponent(val project: Project) : AbstractProjectComponent(project), ILifetimedComponent by LifetimedComponent(project) {
-    companion object {
-        val logger = getLogger(this)
-    }
     override fun projectOpened() {
 
         val csprojFile = Files.walk(File(project.baseDir.path).toPath()).filter({
@@ -41,8 +31,8 @@ class UnityConnectorProjectComponent(val project: Project) : AbstractProjectComp
 
             if (pid != null) {
                 val unityConfig = UnityLocalAttachConfiguration("temp", pid.toInt())
-                var runManager = RunManagerEx.getInstance(project)
-                var config = runManager.createRunConfiguration("unity-debug-generated", DotNetRemoteConfigurationFactory(MonoRemoteConfigType()))
+                val runManager = RunManagerEx.getInstance(project)
+                val config = runManager.createRunConfiguration("unity-debug-generated", DotNetRemoteConfigurationFactory(MonoRemoteConfigType()))
                 (config.configuration as DotNetRemoteConfiguration).address = unityConfig.address
                 (config.configuration as DotNetRemoteConfiguration).port = unityConfig.port
                 config.isTemporary = true
