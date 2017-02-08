a70e83fda66311f94026893711bc28bb89b5c612
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2017-02-08 15:30:05 +0100

    switch to unityProcessId set inside csproj instead of env var

--- src/com/potter/riderunity3dconnector/UnityConnectorProjectComponent.kt
+++ src/com/potter/riderunity3dconnector/UnityConnectorProjectComponent.kt
@@ -13,6 +13,10 @@ import com.jetbrains.resharper.util.idea.LifetimedComponent
 import com.jetbrains.resharper.util.idea.getLogger
 import java.io.File
 import java.nio.file.Files
+import java.nio.file.Path
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.xpath.XPathConstants
+import javax.xml.xpath.XPathFactory
 
 /**
  * Created by Ivan.Shakhov on 03.02.2017.
@@ -24,21 +28,29 @@ class UnityConnectorProjectComponent(val project: Project) : AbstractProjectComp
         val logger = getLogger(this)
     }
     override fun projectOpened() {
-        val pid = System.getenv("unityProcessId");
-        val referencesUnity = Files.walk(File(project.baseDir.path).toPath()).filter({ a -> a.isFile() && a.toFile().extension == "csproj" && a.toFile().readText().contains("UnityEngine.dll")}).count()>0
-        if (pid!=null && referencesUnity) {
-            var id = Integer.valueOf(pid)
-            val unityConfig = UnityLocalAttachConfiguration("temp", id)
-            var runManager = RunManagerEx.getInstance(project)
-            var config = runManager.createRunConfiguration("unity-debug-generated", DotNetRemoteConfigurationFactory(MonoRemoteConfigType()))
-            (config.configuration as DotNetRemoteConfiguration).address = unityConfig.address
-            (config.configuration as DotNetRemoteConfiguration).port = unityConfig.port
-            config.isTemporary=true
-            runManager.addConfiguration(config, false)
-            runManager.selectedConfiguration = config
+
+        val csprojFile = Files.walk(File(project.baseDir.path).toPath()).filter({
+            it.isFile() && it.toFile().extension == "csproj"
+                    && it.toFile().readText().contains("UnityEngine.dll") && it.toFile().readText().contains("<unityProcessId>")
+        }).findFirst()
+        if (csprojFile.isPresent) {
+            val pid =
+                    XPathFactory.newInstance().newXPath().compile("/Project/PropertyGroup/unityProcessId")
+                            .evaluate(DocumentBuilderFactory.newInstance()
+                                    .newDocumentBuilder().parse(csprojFile.get().toFile()), XPathConstants.NUMBER) as Double?
+
+            if (pid != null) {
+                val unityConfig = UnityLocalAttachConfiguration("temp", pid.toInt())
+                var runManager = RunManagerEx.getInstance(project)
+                var config = runManager.createRunConfiguration("unity-debug-generated", DotNetRemoteConfigurationFactory(MonoRemoteConfigType()))
+                (config.configuration as DotNetRemoteConfiguration).address = unityConfig.address
+                (config.configuration as DotNetRemoteConfiguration).port = unityConfig.port
+                config.isTemporary = true
+                runManager.addConfiguration(config, false)
+                runManager.selectedConfiguration = config
+            }
         }
     }
-
     override fun getComponentName(): String {
         return "UnityConnectorProjectComponent"
     }
